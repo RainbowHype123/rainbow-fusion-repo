18b2aae9c1d365a9be59be8608ea5a0ece302516
RainbowHype123 <dplayer1230@gmail.com>
2026-09-03 01:22:52 -0700

    Add storefloat XNA
    I am close to being able to test XNA ports. So, expect some patches for those soon.

---
+++ Extension Objects/Runtime Ports/_Source Codes (XNA & Java)/XNA/CRunstorefloat.cs
@@ -0,0 +1,68 @@
+﻿// storefloat Object - XNA port (All Platforms)
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RuntimeXNA.Extensions;
+using RuntimeXNA.Services;
+using RuntimeXNA.RunLoop;
+using RuntimeXNA.Sprites;
+using RuntimeXNA.Conditions;
+using RuntimeXNA.Actions;
+using RuntimeXNA.Expressions;
+using RuntimeXNA.Objects;
+using RuntimeXNA.Params;
+using RuntimeXNA.Frame;
+using RuntimeXNA.OI;
+using RuntimeXNA.Movements;
+
+namespace RuntimeXNA.Extensions
+{
+    class CRunstorefloat : CRunExtension
+    {
+        //const int CND_LAST = 0;
+
+        const int EXP_FLOATTOLONG = 0;
+        const int EXP_LONGTOFLOAT = 1;
+
+        public override int getNumberOfConditions()
+        {
+            return 0;
+        }
+
+        public override bool createRunObject(CFile file, CCreateObjectInfo cob, int version)
+        {
+            return true;
+        }
+
+        public override bool condition(int num, CCndExtension cnd)
+        {
+            return false;
+        }
+
+        public override void action(int num, CActExtension act)
+        {
+            // No actions
+        }
+
+        public override CValue expression(int num)
+        {
+            switch (num)
+            {
+                case EXP_FLOATTOLONG:
+				{
+                    double value = ho.getExpParam().GetDouble();
+
+                    return new CValue(BitConverter.SingleToInt32Bits(value));
+                }
+                case EXP_LONGTOFLOAT:
+				{
+                    int value = ho.getExpParam().GetInt();
+
+                    return new CValue(BitConverter.Int32BitsToSingle(value));
+                }
+            }
+            return new CValue(0);
+        }
+    }
+}
